f7c98f088fc5b3a2f00a5d4992ee45d2402ff7dd
hemilrajpura-simform <hemil.r@simformsolutions.com>
2022-01-11 14:29:48 +0530

    interfaces

--- src/script.ts
+++ src/script.ts
@@ -1,19 +1,32 @@
 console.log("hello");
 
-var a:number = 12;
-var b:string = "hello world";
+var a: number = 12;
+var b: string = "hello world";
 console.log(b);
 
 /* arrays */
 
-var aa: number[]=[];
+var aa: number[] = [];
 aa.push(12);
 
 
 /* tuple */
 
-let oneTuple : [number,string] = [
-158,
-"Hemil Rajpura"    
+let oneTuple: [number, string] = [
+    158,
+    "Hemil Rajpura"
 ];
+
+let twoTuple: [rollNumber: number, Studentname?: string] = [
+    158
+];   // '?' means Studentname maybe there or may not be there
+
+
+/* Interafces */
+interface Details {
+    userId : number;
+    userName ?: string;
+}
+
+let ee: Details ={userId:158,userName:"hemil"};
 
