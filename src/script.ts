ddc3745363cda1608f73e154dee32269d76a305e
hemilrajpura-simform <hemil.r@simformsolutions.com>
2022-01-12 10:19:15 +0530

    abstract classes

--- src/script.ts
+++ src/script.ts
@@ -24,26 +24,26 @@ let twoTuple: [rollNumber: number, Studentname?: string] = [
 
 /* Interafces */
 interface Details {
-    userId : number;
-    userName ?: string;
+    userId: number;
+    userName?: string;
 }
 
-let ee: Details ={userId:158,userName:"hemil"};
+let ee: Details = { userId: 158, userName: "hemil" };
 
 /* Union types */
 export interface phone {
-    name:string;
-    number:number;
+    name: string;
+    number: number;
 }
 export interface email {
-    name:string;
-    email:string;
+    name: string;
+    email: string;
 }
 
-let unionInterface : email & phone = {
-name:"hemil",
-email:"hemil.r",
-number:780
+let unionInterface: email & phone = {
+    name: "hemil",
+    email: "hemil.r",
+    number: 780
 };
 console.log(unionInterface.email);
 
@@ -51,8 +51,8 @@ console.log(unionInterface.email);
 
 /* functions */
 
-function println(str:string){
-console.log(str);
+function println(str: string) {
+    console.log(str);
 }
 
 println("Hello by funciton");
@@ -63,15 +63,54 @@ println("Hello by funciton");
 
 type StringOrNumber = string | number;
 
-type HasName = {name : string};
+type HasName = { name: string };
 
 // extends
 // how a interface can extends other one
 interface CountryPhoneNumber {
-code:number;
- contact:number;
+    code: number;
+    contact: number;
 }
 
-export interface InternationalPhoneNumbers extends CountryPhoneNumber{
+export interface InternationalPhoneNumbers extends CountryPhoneNumber {
     countryCode: string;
 }
+
+// classes
+export class emplyoee {
+    public email: string;
+    public name: string;
+
+    constructor(name: string, email: string) {
+        this.email = email;
+        this.name = name;
+    }
+}
+
+//abstract class
+abstract class AbsClass {
+    public abstract phone: number;
+
+    constructor(
+        public name: string
+    ) { this.name = name; }
+    abstract sendSms(): void; //it must be implemented by any child class
+}
+
+
+class childClass extends AbsClass {
+    phone:number;
+    constructor(name:string) {
+        super(name);
+    }
+    sendSms(): void {
+        console.log("child class implemented");
+        this.phone = 5;
+    }
+}
+
+const obj = new childClass("hemil");
+console.log(obj.phone);
+obj.sendSms();
+console.log(obj.phone);
+
